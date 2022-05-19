df13d79bb6e37813065f023542969b313cc8cb05
Require <68991107+1e17@users.noreply.github.com>
2022-05-18 18:54:36 -0700

    Create event.lua

---
+++ event.lua
@@ -0,0 +1,36 @@
+
+local event = {}
+
+function event.new()
+  local self = {}
+  self.listeners = {}
+
+  function self:connect(callback)
+    local listener = {}
+    listener.callback = callback
+    listener.id = #self.listeners + 1
+    self.listeners[listener.id] = listener
+
+    function listener:disconnect()
+      self.listeners[listener.id] = nil
+    end 
+
+    return listener
+  end 
+
+  function self:close()
+    self.listeners = nil 
+    self = nil
+  end 
+
+  function self:fire(...)
+    for i = 1, #self.listeners do
+      local listener = self.listeners[i]
+      listener.callback(...)
+    end
+  end 
+
+  return self
+end 
+
+return event
